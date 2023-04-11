9bb19bf0e1b8be6c05b62b4b44ce51294498cfa6
Dobroiu Anamaria <74797999+DobroiuAnamaria@users.noreply.github.com>
2023-04-11 21:28:12 +0300

    updated packages

--- Frontend/AngularApplication/src/app/app.module.ts
+++ Frontend/AngularApplication/src/app/app.module.ts
@@ -1,5 +1,3 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +7,9 @@ import { LoginFormComponent } from './login/login-form/login-form.component';
 import { RegisterComponent } from './register/register.component';
 import { RegisterFormComponent } from './register/register-form/register-form.component';
 import { UserService } from './user.service';
-import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
